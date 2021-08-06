c158a04e0f10e4c1bb1f05d882e6c4b4c443826b
cyberta <cyberta@riseup.net>
2021-08-06 15:27:50 +0200

    adapt dummy password, write some hints on main fragment, copy email account to clipboard

--- app/src/main/java/org/cyberta/YggmailService.java
+++ app/src/main/java/org/cyberta/YggmailService.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 import yggmail.Yggmail_;
 
@@ -58,13 +59,20 @@ public class YggmailService extends Service {
         Log.d(TAG, getApplicationContext().getFilesDir().getPath()+"/yggmail.db");
         yggmail.setDatabaseName(getApplicationContext().getFilesDir().getPath()+"/yggmail.db");
 
-        yggmail.createPasswordLogError("1324asdf");
+        yggmail.createPasswordLogError("delta");
         yggmail.startLogError("localhost:1025",
                 "localhost:1143",
                 false,
                 "tcp://45.138.172.192:5001,tcp://94.130.203.208:5999,tcp://bunkertreff.ddns.net:5454,tcp://ygg.mkg20001.io:80,tcp://yugudorashiru.de:80");
 
-
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            Toast.makeText(getApplicationContext(), "account name " + yggmail.getAccountName() + "@yggmail copied to clipboard", Toast.LENGTH_LONG ).show();
+            Util.writeTextToClipboard(getApplicationContext(), yggmail.getAccountName() + "@yggmail");
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 

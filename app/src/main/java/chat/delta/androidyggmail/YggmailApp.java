2cc5bf9d351c11000beeb0d090aa21ac1826db55
cyberta <cyberta@riseup.net>
2022-03-03 21:51:24 +0100

    introduce NetworkStateManager and observe wifi and general internet connectivity

--- app/src/main/java/chat/delta/androidyggmail/YggmailApp.java
+++ app/src/main/java/chat/delta/androidyggmail/YggmailApp.java
@@ -5,10 +5,12 @@ import android.app.Application;
 public class YggmailApp extends Application {
 
     private YggmailObservable yggmailObservable;
+    private NetworkStateManager networkStateManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
         this.yggmailObservable = YggmailObservable.getInstance();
+        this.networkStateManager = new NetworkStateManager(this);
     }
 }
--- app/src/main/java/chat/delta/androidyggmail/YggmailObservable.java
+++ app/src/main/java/chat/delta/androidyggmail/YggmailObservable.java
@@ -1,5 +1,6 @@
 package chat.delta.androidyggmail;
 
+import java.util.HashSet;
 import java.util.Observable;
 
 public class YggmailObservable extends Observable {
@@ -11,9 +12,26 @@ public class YggmailObservable extends Observable {
         ShuttingDown,
         Error
     }
+
+    public enum WifiStatus {
+        Connected,
+        Disconnected
+    }
+    public enum NetworkStatus {
+        Connected,
+        Disconnected
+    }
+
+    public HashSet<String> localPeers;
+    public HashSet<String> publicPeers;
+
     private Status status = Status.Stopped;
+    private WifiStatus wifiStatus = WifiStatus.Disconnected;
+    private NetworkStatus networkStatus = NetworkStatus.Disconnected;
 
     private YggmailObservable() {
+        localPeers = new HashSet<>();
+        publicPeers = new HashSet<>();
     }
 
     public static YggmailObservable getInstance() {
@@ -29,6 +47,30 @@ public class YggmailObservable extends Observable {
             setChanged();
             notifyObservers();
         }
+    }
+
+    public void setNetworkStatus(NetworkStatus networkStatus) {
+        if (networkStatus != this.networkStatus) {
+            this.networkStatus = networkStatus;
+            setChanged();
+            notifyObservers();
+        }
+    }
+
+    public void setWifiStatus(WifiStatus wifiStatus) {
+        if (wifiStatus != this.wifiStatus) {
+            this.wifiStatus = wifiStatus;
+            setChanged();
+            notifyObservers();
+        }
+    }
+
+    public NetworkStatus getNetworkStatus() {
+        return networkStatus;
+    }
+
+    public WifiStatus getWifiStatus() {
+        return wifiStatus;
     }
 
     public Status getStatus() {
